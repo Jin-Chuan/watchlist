fbeb9b6d1edeae653806d9bd6869557278325db9
JinChuan <447154317@qq.com>
2021-11-29 20:07:32 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask,url_for, escape
+from flask import Flask,url_for, render_template,escape
 app=Flask(__name__)
 # ...
+name = 'Jin Chuan'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 @app.route('/')
-def hello():
-    return 'Hello'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
     return 'User: %s' % escape(name)
+
+@app.route('/user/<int:number>')
+def user_number(number):
+    return str(number)
 
 @app.route('/test')
 def test_url_for():
@@ -21,3 +38,4 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
