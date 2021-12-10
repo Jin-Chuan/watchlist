6d133d89608b2ec85f583bace2fb39d0a6852def
JinChuan <447154317@qq.com>
2021-12-11 00:00:22 +0800

    Organize application with package

---
+++ watchlist/__init__.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+app.config['SECRET_KEY'] = 'dev'
+# 注意更新这里的路径，把 app.root_path 添加到 os.path.dirname() 中
+# 以便把文件定位到项目根目录
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db=SQLAlchemy(app)
+login_manager = LoginManager(app)  # 实例化扩展类
+   
+@login_manager.user_loader
+def load_user(user_id):  # 创建用户加载回调函数，接受用户 ID 作为参数
+    from watchlist.models import User
+    user = User.query.get(int(user_id))
+    return user
+
+login_manager.login_view = 'login'#虽然登录视图函数 定义在views模块中 但这里只是赋值 没有关系？
+
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    from watchlist.models import User
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+
+from watchlist import views, errors, commands    
